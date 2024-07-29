a670e4544fbd9d2da82a0914906366fa16784161
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-07-29 08:36:00 +0000

    投稿フォーム作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
   
+  has_many :post, dependent: :destroy
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
   get 'posts/edit'
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit]
+  resources :posts, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
