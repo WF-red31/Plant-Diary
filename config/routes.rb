f46ac7a0f90a74f47e23f649bbf77885df46750b
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-07-30 07:06:21 +0000

    編集、削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit]
-  resources :posts, only: [:new, :create, :index, :show]
+  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
