dec00a8f938bacc605bbbf5d5a00ba01261d68c9
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-07-30 08:10:25 +0000

    フラッシュメッセージの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
+      flash[:notice] = "新規投稿の作成に成功しました。"
       redirect_to posts_path
     else
       render :new
@@ -28,6 +29,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
+      flash[:notice] = "投稿の更新に成功しました。"
       redirect_to post_path(@post.id)
     else
       render :edit
@@ -37,6 +39,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = "投稿の削除に成功しました。"
     redirect_to posts_path
   end
   
