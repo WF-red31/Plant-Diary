fb08dfff252f30a934328e438fd7b79d4d3bda03
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-07-31 11:29:04 +0000

    ユーザーページの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  has_one_attached :profile_image
+  has_many :posts, dependent: :destroy
+
   validates :name, presence: true
   validates :name, length: {in: 2..20}
   validates :name, uniqueness: true
-  validates :email, presence: true
-  validates :password, presence: true
   
-  has_many :post, dependent: :destroy
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_profile.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
   
 end
