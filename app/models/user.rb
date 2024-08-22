b8b2b2e36a212a2fa3410df28d2d3bc1defb7508
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-08-22 05:43:55 +0000

    commentモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :name, length: {in: 2..20}
