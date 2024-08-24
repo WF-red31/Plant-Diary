5273430bbe5385c255cf22bb2caed44efad20b05
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-08-24 08:24:31 +0000

    コメントの投稿、削除、レイアウト整理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update, :destroy, :index]
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
