fa1979deb61654b08dcdc2b47eeb08871a7bf6b2
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-03 04:17:09 +0000

    検索機能の実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,14 +6,14 @@ class SearchesController < ApplicationController
       @posts = []
       if params[:model] == 'post'
         @posts = Post.all
-        if params[keyword].present?
-          @posts = @posts.where('content LIKE ?', "%#{params[:keyword]}%")
+        if params[:keyword].present?
+          @posts = @posts.where('title LIKE ?', "%#{params[:keyword]}%")
         end
       elsif params[:model] == 'user'
         @users = User.all
           if params[:keyword].present?
             @users = @users.where('name LIKE ?', "%#{params[:keyword]}%").or(
-             @users.where('email LIKE ?', "%#{params[:keyword]}%")
+             @users.where('email LIKE ?', "%#{params[:keyword]}%"))
           end
       end
     end
