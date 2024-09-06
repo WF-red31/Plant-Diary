737b1f5782fa8a14367312b815be7d748b688a70
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-06 07:38:30 +0000

    publicフォルダの作成と編集

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,17 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
   
-  get 'posts/edit'
-  devise_for :users
-  root to: 'homes#top'
-  get 'about' => 'homes#about', as: 'about'
-  resources :users, only: [:show, :edit, :update, :destroy, :index]
-  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-    resources :comments, only: [:create, :destroy]
-  end
+  scope module: :public do
+    get 'posts/edit'
+    devise_for :users
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+    resources :users, only: [:show, :edit, :update, :destroy, :index]
+    resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+      resources :comments, only: [:create, :destroy]
+    end
   
-  get 'search' => 'searches#search'
+    get 'search' => 'searches#search'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
