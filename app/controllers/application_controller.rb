4957c0e531424772fedf193de17f6f1bd20e505c
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-06 07:55:22 +0000

    ユーザー側のdeviceコントローラー生成とrenderの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about], unless: :admin_controller?
-  before_action :configure_permitted_parameters, if: :devise_controller?
   
-  def after_sign_in_path_for(resource)
-    user_path(current_user.id)
-  end
   
-  def after_sign_up_path_for(resource)
-    user_path(current_user.id)
-  end
   
-  def after_sign_out_path_for(resource)
-    root_path
-  end
   
   private
  
@@ -20,9 +10,5 @@ class ApplicationController < ActionController::Base
     self.class.module_parent_name == 'Admin'
   end
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  
 end
