770778f9ec98ef5483f642384758fc89fac48d5c
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-06 07:20:49 +0000

    管理者のダッシュボード、ログアウト、ユーザー削除機能、権限の設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
   get 'posts/edit'
   devise_for :users
   root to: 'homes#top'
