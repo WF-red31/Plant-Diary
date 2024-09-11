a1f30d6d46c32236225a158297191b1b33163379
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-11 06:30:33 +0000

    fakerの導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(email: 'admin@example.com' , password: 'password' )
+
+(1..10).each do |i|
+  User.create!(
+    name: Faker::Name.name,
+    email: "user#{i}@example.com",
+    password: "password"
+  )
+end
+
+if Rails.evn.development?
+  users = User.all.limit(3)
+  
+  users.each_with_index do |user, n|
+    3.downto(0) do |i|
+      user.posts.create!(
+        title: Faker::Lorem.word,
+        body: Faker::Lorem.sentence,
+        date: Time.current.ago((n + i * 2).day)
+      )
+    end
+  end
+  
+  posts = Post.all
+  
+  User.all.sample(6).each do |user|
+    user.comments.create!(
+      comment: Faker::Lorem.sentence,
+      post_id: posts.sample.id
+    )
+  end
+end
