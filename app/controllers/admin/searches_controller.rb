82a9e11b965e676134a186ddf30bd833d75bf077
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-13 08:01:15 +0000

    searchメソッドの定義

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -2,7 +2,20 @@ class Admin::SearchesController < ApplicationController
   before_action :authenticate_user!
   
   def search
-    
+    @users = []
+    @posts = []
+      if params[:model] == 'post'
+        @posts = Post.all
+        if params[:keyword].present?
+          @posts = @posts.where('title LIKE ?', "%#{params[:keyword]}%")
+        end
+      elsif params[:model] == 'user'
+        @users = User.all
+          if params[:keyword].present?
+            @users = @users.where('name LIKE ?', "%#{params[:keyword]}%").or(
+             @users.where('email LIKE ?', "%#{params[:keyword]}%"))
+          end
+      end
   end
   
 end
