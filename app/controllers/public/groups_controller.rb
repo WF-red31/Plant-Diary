aac219bbd49ba628c19d9f2192a08160da348f1b
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-09-25 10:09:33 +0000

    グループの編集、更新機能の追加

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -25,6 +25,20 @@ class Public::GroupsController < ApplicationController
     @post = @group.posts.page(params[:page])
   end
   
+  def edit
+    @group = Group.find(params[:id])
+  end
+  
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(user_params)
+      flash[:notice] = "グループの更新に成功しました。"
+      redirect_to group_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
   private
   
   def group_params
