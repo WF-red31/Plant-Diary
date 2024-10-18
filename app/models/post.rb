0dcac5bc335d6f1e969bc037fdbff45243957a76
EC2 Default User <169152935+WF-red31@users.noreply.github.com>
2024-10-18 06:48:53 +0000

    fix activestrage return

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,6 +24,7 @@ class Post < ApplicationRecord
           image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
       end
       image.variant(resize_to_limit: [width, height])
+      image
   end 
   
   private
